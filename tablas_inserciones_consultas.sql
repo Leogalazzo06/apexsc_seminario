ec60efee8af657503612877ac3868e06cb37e36e
Leogalazzo06 <legalazzo@soysiglo.21.edu.ar>
2025-11-16 20:37:15 -0300

    Update tablas_inserciones_consultas.sql

--- tablas_inserciones_consultas.sql
+++ tablas_inserciones_consultas.sql
@@ -1,121 +1,128 @@
 -- TABLA USUARIO
-CREATE TABLE Usuario (
-    id_usuario SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    rol VARCHAR(20) NOT NULL
+CREATE TABLE public.usuario (
+    id_usuario integer NOT NULL,
+    username character varying(50) NOT NULL,
+    password character varying(255) NOT NULL,
+    rol character varying(20) NOT NULL
 );
 
 -- TABLA SOCIO 
-CREATE TABLE Socio (
-    id_socio SERIAL PRIMARY KEY,
-    dni VARCHAR(15) NOT NULL UNIQUE,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    estado VARCHAR(20) NOT NULL,
-    id_usuario INT NOT NULL REFERENCES usuario(id_usuario)
+CREATE TABLE public.socio (
+    id_socio integer NOT NULL,
+    dni character varying(15) NOT NULL,
+    nombre character varying(50) NOT NULL,
+    apellido character varying(50) NOT NULL,
+    estado character varying(20) NOT NULL,
+    id_usuario_creador integer,
+    categoria character varying(20),
+    periodicidad character varying(20) DEFAULT 'Mensual'::character varying,
+    foto_ruta character varying(255)
 );
 
 -- TABLA EVENTO  
-CREATE TABLE Evento (
-    id_evento SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    fecha_hora TIMESTAMP NOT NULL,
-    aforo INT NOT NULL,
-    id_usuario INT NOT NULL REFERENCES usuario(id_usuario)
+CREATE TABLE public.evento (
+    id_evento integer NOT NULL,
+    nombre character varying(100) NOT NULL,
+    fecha_hora timestamp without time zone NOT NULL,
+    aforo integer NOT NULL,
+    id_usuario integer NOT NULL,
+    lugar character varying(100),
+    precio_general numeric(10,2),
+    precio_socio numeric(10,2),
+    precio_invitado numeric(10,2)
 );
+
 
 -- TABLA ENTRADA  
-CREATE TABLE entrada (
-    id_entrada SERIAL PRIMARY KEY,
-    codigo_qr VARCHAR(100) NOT NULL UNIQUE,
-    precio NUMERIC(10,2) NOT NULL,
-    estado_uso VARCHAR(20) NOT NULL,
-    id_evento INT NOT NULL REFERENCES evento(id_evento),
-    id_socio INT NOT NULL REFERENCES socio(id_socio)
+CREATE TABLE public.entrada (
+    id_entrada integer NOT NULL,
+    id_evento integer NOT NULL,
+    nombre_comprador character varying(100),
+    dni character varying(20),
+    tipo character varying(30),
+    precio numeric(10,2),
+    codigo_qr text NOT NULL,
+    usado boolean DEFAULT false,
+    fecha_compra timestamp without time zone DEFAULT now()
 );
+
 
 -- TABLA CUOTA 
-CREATE TABLE cuota (
-    id_cuota SERIAL PRIMARY KEY,
-    periodo VARCHAR(20) NOT NULL,
-    monto NUMERIC(10,2) NOT NULL,
-    estado VARCHAR(20) NOT NULL,
-    id_socio INT NOT NULL REFERENCES socio(id_socio)
+CREATE TABLE public.cuota (
+    id_cuota integer NOT NULL,
+    periodo character varying(20) NOT NULL,
+    monto numeric(10,2) NOT NULL,
+    estado character varying(20) NOT NULL,
+    id_socio integer NOT NULL,
+    pagada boolean DEFAULT false
 );
+
 
 -- TABLA PAGO 
-CREATE TABLE pago (
-    id_pago SERIAL PRIMARY KEY,
-    fecha DATE NOT NULL,
-    monto NUMERIC(10,2) NOT NULL,
-    metodo VARCHAR(20) NOT NULL,
-    id_cuota INT NOT NULL REFERENCES cuota(id_cuota)
+CREATE TABLE public.pago (
+    id_pago integer NOT NULL,
+    fecha date NOT NULL,
+    monto numeric(10,2) NOT NULL,
+    metodo character varying(20) NOT NULL,
+    id_cuota integer NOT NULL
 );
+
 
 -- TABLA ABONOPILETA  
-CREATE TABLE abonopileta (
-    id_abono SERIAL PRIMARY KEY,
-    tipo VARCHAR(50) NOT NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL,
-    id_socio INT NOT NULL REFERENCES socio(id_socio)
+CREATE TABLE public.abono_pileta (
+    id_abono integer NOT NULL,
+    id_socio integer NOT NULL,
+    fecha_inicio date NOT NULL,
+    fecha_fin date NOT NULL,
+    precio numeric(10,2) NOT NULL,
+    estado character varying(20) DEFAULT 'Vigente'::character varying NOT NULL,
+    tipo character varying(20) DEFAULT 'Individual'::character varying,
+    estado_pago character varying(20) DEFAULT 'Pendiente'::character varying
 );
 
--- INSERCIÓN DE DATOS
+--Inserción tablas 
 
--- Insertar datos en «Usuario» 
-INSERT INTO Usuario (id_usuario, username, password_hash, rol)
-VALUES
-(1, 'admin', 'hash_admin', 'Administrador'),
-(2, 'cajero1', 'hash_cajero1', 'Cajero');
+--abono_pileta
+COPY public.abono_pileta (id_abono, id_socio, fecha_inicio, fecha_fin, precio, estado, tipo, estado_pago) FROM stdin;
+9	40	2025-04-06	2025-12-08	5000.00	Vigente	FAMILIAR	Pagado
+\.
 
--- Insertar datos en «Socio» c
-INSERT INTO Socio (id_socio, dni, nombre, apellido, estado, id_usuario)
-VALUES
-(1, '44555666', 'Cole', 'Palmer', 'Activo', 1),
-(2, '44888999', 'Vitor', 'Roque', 'Activo', 1);
+--cuota
+COPY public.cuota (id_cuota, periodo, monto, estado, id_socio, pagada) FROM stdin;
+19	2025-01-01	15000.00	Pagada	37	t
+20	2025-01-01	15000.00	Pagada	44	t
+21	2025-01-01	15000.00	Pendiente	33	f
+\.
 
--- Insertar datos en «Evento» 
-INSERT INTO Evento (id_evento, nombre, fecha_hora, aforo, id_usuario)
-VALUES
-(1, 'Apex SC vs. Crystal Oaks', '2025-10-05 16:00:00', 5000, 1);
+--entrada
+COPY public.entrada (id_entrada, id_evento, nombre_comprador, dni, tipo, precio, codigo_qr, usado, fecha_compra) FROM stdin;
+12	11	Leonel	45103729	Palco	9000.00	66b7d776-1354-4083-916d-e1f3ef672368	t	2025-11-16 18:47:15.400254
+\.
 
--- Insertar datos en «Cuota» 
-INSERT INTO Cuota (id_cuota, periodo, monto, estado, id_socio)
-VALUES
-(1, 'Octubre 2025', 5000.00, 'Pendiente', 1),
-(2, 'Octubre 2025', 5000.00, 'Pagada', 2);
+--evento
+COPY public.evento (id_evento, nombre, fecha_hora, aforo, id_usuario, lugar, precio_general, precio_socio, precio_invitado) FROM stdin;
+11	Argentina Vs. Brasil	2025-12-23 00:00:00	52000	9	La Bombonera	7900.00	7000.00	9000.00
+\.
 
--- Insertar datos en «Pago» 
-INSERT INTO Pago (id_pago, fecha, monto, metodo, id_cuota)
-VALUES
-(1, '2025-10-01', 5000.00, 'Efectivo', 2);
+--pago
+COPY public.pago (id_pago, fecha, monto, metodo, id_cuota) FROM stdin;
+\.
 
--- Insertar datos en «AbonoPileta» 
-INSERT INTO AbonoPileta (id_abono, tipo, fecha_inicio, fecha_fin, id_socio)
-VALUES
-(1, 'Mensual', '2025-10-01', '2025-10-31', 1);
+--socio
+COPY public.socio (id_socio, dni, nombre, apellido, estado, id_usuario_creador, categoria, periodicidad, foto_ruta) FROM stdin;
+33	35678987	Cristian	Romero	Activo	9	Adulto	Mensual	NULL
+36	31234567	Rodrigo	De Paul	Activo	9	Adulto	Mensual	NULL
+37	34789654	Alexis	Mac Allister	Activo	9	Adulto	Mensual	NULL
+38	44234432	Enzo	Fernandez	Activo	9	Adulto	Mensual	NULL
+39	32123456	Julián	Alvarez	Activo	9	Adulto	Mensual	NULL
+40	33245678	Ángel	Di Maria	Activo	9	Adulto	Mensual	NULL
+43	32456789	Lionel	Messi	Activo	9	Adulto	Mensual	NULL
+44	45103729	Leonel	Galazzo	Activo	9	Adulto	Mensual	NULL
+\.
 
--- Insertar datos en «Entrada» 
-INSERT INTO Entrada (id_entrada, codigo_qr, precio, estado_uso, id_evento, id_socio)
-VALUES
-(1, 'QR1234567890', 2000.00, 'No usada', 1, 1),
-(2, 'QR9876543210', 2000.00, 'No usada', 1, 2);
+--usuario
+COPY public.usuario (id_usuario, username, password, rol) FROM stdin;
+9	Admin	admin	Administrador
+13	Caja	1234	Cajero
+\.
 
---CONSULTA DE DATOS 
-SELECT id_socio, nombre, apellido, dni
-FROM Socio 
-WHERE dni = '44555666'; 
-
-SELECT p.id_pago, s.nombre, s.apellido, p.monto, p.metodo
-FROM Pago p
-JOIN Cuota c ON p.id_cuota = c.id_cuota
-JOIN Socio s ON c.id_socio = s.id_socio
-WHERE p.fecha =  '2025-10-01'; 
-
-SELECT e.nombre AS evento, s.nombre AS socio, s.apellido, en.codigo_qr, en.estado_uso
-FROM Entrada en 
-JOIN Evento e ON en.id_evento = e.id_evento
-JOIN Socio s ON en.id_socio = s.id_socio
-WHERE e.nombre LIKE '%Apex SC%'; 
